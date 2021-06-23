8d5257036b61bae837f9cb504a9ff1c40d125058
Stefan Her <stefan.her@gmail.com>
2021-06-23 01:20:26 -0700

    DAO CRUD

--- app/src/main/java/be/stefan/event/db/EventDao.kt
+++ app/src/main/java/be/stefan/event/db/EventDao.kt
@@ -2,6 +2,7 @@ package be.stefan.event.db
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import be.stefan.event.models.Event
 
@@ -35,6 +36,16 @@ class EventDao {
 
     // CRUD
 
+    private fun cursor2Item(cursor : Cursor) : Event {
+        val id = cursor.getLong(cursor.getColumnIndex(DbInfo.COLUMN_ID))
+        val title = cursor.getString(cursor.getColumnIndex(DbInfo.COLUMN_TITLE))
+        val time = cursor.getString(cursor.getColumnIndex(DbInfo.COLUMN_TIME))
+        val desc = cursor.getString(cursor.getColumnIndex(DbInfo.COLUMN_DESC))
+        val address = cursor.getString(cursor.getColumnIndex(DbInfo.COLUMN_ADDRESS))
+        val item = Event(id, title, time, desc, address)
+        return item
+    }
+
     private fun createContentValues(item : Event) : ContentValues {
         val contentValues = ContentValues()
         contentValues.put(DbInfo.COLUMN_TITLE, item.title)
@@ -49,5 +60,70 @@ class EventDao {
         return db.insert(DbInfo.TABLE_NAME, null, contentValues)
     }
 
+    fun readItem(id : Long) : Event? {
+        val cursor = db.query(
+            DbInfo.TABLE_NAME,
+            null,
+            DbInfo.COLUMN_ID + "= ?",
+            arrayOf(id.toString()),
+            null,
+            null,
+            null
+        )
 
+        if (cursor.count > 0) {
+            cursor.moveToFirst()
+            return cursor2Item(cursor)
+        }
+
+        return null
+    }
+
+    fun allItems() : List<Event>? {
+        val cursor = db.query(
+            DbInfo.TABLE_NAME,
+            null,
+            "datetime(${DbInfo.COLUMN_TIME}) > datetime('now')",
+            null,
+            null,
+            null,
+            DbInfo.COLUMN_TIME +" ASC"
+        )
+
+        if (cursor.count > 0) {
+            cursor.moveToFirst()
+            val list : MutableList<Event> = mutableListOf()
+            while (!cursor.isAfterLast) {
+                list.add(cursor2Item(cursor))
+                cursor.moveToNext()
+            }
+            return list.toList()
+        }
+
+        return null
+    }
+
+    fun update(id : Long) : Boolean {
+        val contentValues = ContentValues()
+        val nbRow = db.update(
+            DbInfo.TABLE_NAME,
+            contentValues,
+            DbInfo.COLUMN_ID + " = ?",
+            arrayOf(id.toString())
+        )
+
+        if (nbRow == 1) { return true }
+        return false
+    }
+
+    fun delete(id : Long) : Boolean {
+        val nbRow = db.delete(
+            DbInfo.TABLE_NAME,
+            DbInfo.COLUMN_ID + " = ?",
+            arrayOf(id.toString())
+        )
+
+        if (nbRow == 1) { return true }
+        return false
+    }
 }
--- app/src/main/java/be/stefan/event/models/Event.kt
+++ app/src/main/java/be/stefan/event/models/Event.kt
@@ -2,13 +2,14 @@ package be.stefan.event.models
 
 class Event {
 
-
+    var id : Long
     var title : String
     var desc : String
     var time : String
     var address : String
 
-    constructor( title : String, desc : String, time : String , address: String) {
+    constructor(id : Long, title : String, desc : String, time : String , address: String) {
+        this.id = id
         this.title = title
         this.desc = desc
         this.time = time
